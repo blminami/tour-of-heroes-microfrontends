26b4ab9f6ca7cc547516625de1a26f5500896ae9
Narcisa Minca <minca.narcisa@gmail.com>
2023-02-08 16:59:07 +0200

    optional in-mem-service-api config for microapps

--- apps/dashboard-microapp/src/app/app.module.ts
+++ apps/dashboard-microapp/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from 'core';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
+    // optional: setting in-memory-web-api for microapps so we can test them separately
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
     RouterModule.forRoot(
       [
         {
